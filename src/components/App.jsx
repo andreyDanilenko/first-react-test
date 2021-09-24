b783a025cd4bc063622dbadc677f527f2e376369
Andrey <danilenko.a.g@mail.ru>
2021-09-24 19:21:34 +0300

    Тестируем изменение состояния

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCount } from "../reducers/companyReducer";
+import './app.scss';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(state => state.company.count)
+
+  function onCountClick() {
+    dispatch(setCount(7))
+  };
+
   return (
     <div>
-      React
+      <button onClick={() => onCountClick()}>Count</button>
+      <div>{count}</div>
     </div>
   )
 }
