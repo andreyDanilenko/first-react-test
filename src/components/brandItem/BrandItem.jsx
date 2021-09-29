d1b527208a2e84a63a08cb8a5e3704ae5d0ea7e4
Andrey <danilenko.a.g@mail.ru>
2021-09-29 14:51:30 +0300

    Создаем отрисовку страница бренда

--- src/components/brandItem/BrandItem.jsx
+++ src/components/brandItem/BrandItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './brandItem.scss'
 
-const BrandItem = (props) => {
+const BrandItem = ({ brand, toggle }) => {
+  const history = useHistory()
+
   return (
-    <div className="brand">
-      <div className="brand__percent">{props.brand.bonusSave}</div>
-      <div className="brand__description">от суммы покупки</div>
-      <img className="brand__poster" src={props.brand.poster} />
-      <div className="brand__title">{props.brand.name}</div>
+    <div className="brand" onClick={() => history.push('/partners' + '/' + brand.name)}>
+      <div className="brand__percent">{toggle ? brand.bonusSave : brand.bonusUse}</div>
+      <div className="brand__description">{toggle ? 'от суммы покупки' : 'скидка за бонусы'}</div>
+      <img className="brand__poster" src={brand.poster} />
+      <div className="brand__title">{brand.name}</div>
     </div>
   )
 }
---
+++ src/components/brandPage/BrandPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './brandPage.scss'
+
+const BrandPage = () => {
+  return (
+    <div>
+      <h2>Скидочные купоны · Развлечения</h2>
+
+    </div>
+  )
+}
+
+export default BrandPage;
